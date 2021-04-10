7e0d0159989c5196bec15fb64ca24421092f9624
vijaynath <vijaynath1986@gmail.com>
2021-04-10 15:51:30 +0530

    Revert "screw"
    This reverts commit 22272d08e039e02ebc792fdc388899f1bc6a3c6d.

--- Calculator/main.c
+++ Calculator/main.c
@@ -1,4 +1,6 @@
 #include "stdio.h"
+#include "sub.h"
+
 #include "add.h"
 
 
@@ -6,6 +8,9 @@ int main(){
 
   int res1, res2, res3;
   printf("\nMain function");
+
+  res1 = sub(10, 2);
+  printf("\nSubtraction done - %d", res1);
 
   res3 = add_int(5,6);
   printf("\nAddition integer done - %d", res3);
