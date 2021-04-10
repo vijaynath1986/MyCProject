66470607639b6c4ac4e9e81a34fc2e4fcbd3aa0f
vijaynath <vijaynath1986@gmail.com>
2021-04-10 16:31:19 +0530

    Floating point sub implemented and tested

--- Calculator/main.c
+++ Calculator/main.c
@@ -4,21 +4,23 @@
 
 int main(){
 
+
   int res1, res2, res3;
-  float resf;
+  float resf1, resf2;
   printf("\nMain function");
-
-
-  /*Subtraction */
-  res1 = sub(10, 2);
-  printf("\nSubtraction done - %d", res1);
 
   /*Addition int*/
   res3 = add_int(5,6);
   printf("\nAddition integer done - %d", res3);
 
   /*Addition float*/
-  resf = add_float(5.5,6.6);
-  printf("\nAddition floating point done - %f", resf);
+  resf1 = add_float(5.5,6.6);
+  printf("\nAddition floating point done - %f", resf1);
+
+  res2 = sub_int(10, 2);
+  printf("\nSubtraction integer done - %d", res2);
+
+  resf2 = sub_float(20.22f, 1.12f);
+  printf("\nSubtraction float done - %f", resf2);
 
 }
--- Calculator/sub/sub.c
+++ Calculator/sub/sub.c
@@ -1,5 +1,10 @@
 
 #include "sub.h"
-int sub(int arg1, int arg2){
+int sub_int(int arg1, int arg2){
 	return (arg1-arg2);
 }
+
+float sub_float(float arg1, float arg2){
+	return (arg1-arg2);
+}
+
