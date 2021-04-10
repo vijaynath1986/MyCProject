34366ac45b15ad23141eeb9aef7632afb7e533e2
vijaynath <vijaynath1986@gmail.com>
2021-04-10 16:13:09 +0530

    Added comments

--- Calculator/main.c
+++ Calculator/main.c
@@ -9,10 +9,11 @@ int main(){
   int res1, res2;
   printf("\nMain function");
 
+  /*Subtraction */
   res1 = sub(10, 2);
   printf("\nSubtraction done - %d", res1);
 
-
+  /*Addition */
   res2 = add(5,6);
   printf("\nAddition done - %d", res2);
 
