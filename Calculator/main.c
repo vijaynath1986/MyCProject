3a968d86931074094513c46a43426bf6bbe0fdb9
vijaynath <vijaynath1986@gmail.com>
2021-04-10 15:12:50 +0530

    Added floating point addition feature

--- Calculator/main.c
+++ Calculator/main.c
@@ -5,10 +5,13 @@
 
 int main(){
 
-  int res;
+  int res1, res2;
   printf("\nMain function");
 
-  res = add(5,6);
+  res1 = add_int(5,6);
+  printf("\nAddition integer done - %d", res1);
 
-  printf("\nAddition done - %d", res);
+  res2 = add_float(5.5,6.6);
+  printf("\nAddition floating point done - %d", res2);
+
 }
