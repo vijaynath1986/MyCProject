ffde5697a6c6a36a3d67e451a983070db25dc358
vijaynath <vijaynath1986@gmail.com>
2021-04-13 00:27:05 +0530

    Added comments to float and int sub functions

--- Calculator/main.c
+++ Calculator/main.c
@@ -18,9 +18,11 @@ int main(){
   resf1 = add_float(5.5,6.6);
   printf("\nAddition floating point done - %f", resf1);
 
+  // Integer sub
   res2 = sub_int(10, 2);
   printf("\nSubtraction integer done - %d", res2);
 
+  // Floating point sub
   resf2 = sub_float(20.22f, 1.12f);
   printf("\nSubtraction float done - %f", resf2);
 
--- Calculator/sub/sub.c
+++ Calculator/sub/sub.c
@@ -1,10 +1,12 @@
 
 #include "sub.h"
 int sub_int(int arg1, int arg2){
+	// Integer sub
 	return (arg1-arg2);
 }
 
 float sub_float(float arg1, float arg2){
+	// Floating point sub
 	return (arg1-arg2);
 }
 
